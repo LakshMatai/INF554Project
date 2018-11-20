5e4b7954b3dcd5ac683bdd69f985bd88a819c0e0
Laksh <lakshmatai@live.com>
2018-11-19 16:23:11 -0800

    Added US map and bar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { USMapFlightsComponent } from './us-map-flights/us-map-flights.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    USMapFlightsComponent
   ],
   imports: [
     BrowserModule
