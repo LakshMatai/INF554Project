b932b3f95c866a744c6248b036995f4f02853473
Ashwini Giri <agiri@usc.edu>
2018-11-28 17:23:25 -0800

    working routes navigation in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,23 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { BipartiteComponent } from './bipartite/bipartite.component';
 import { PublicationsComponent } from './publications/publications.component';
 import { AirportConnectivityComponent } from './airport-connectivity/airport-connectivity.component';
+// import {bootstrap} from '@angular/platform-browser';
+// import {provide} from '@angular/core';
+// import {ɵROUTER_PROVIDERS} from '@angular/router';
+// import {LocationStrategy,HashLocationStrategy} from '@angular/common';
+
+// import {MyApp} from './home-page';
+
+// bootstrap(MyApp,[ɵROUTER_PROVIDERS,{provide: LocationStrategy, useClass: HashLocationStrategy}]);
+
 
 const appRoutes:Routes=[
   {path:'us-map-flights',component:USMapFlightsComponent},
   {path:'carrier-stats',component:CarrierStatsComponent},
   {path:'home-page',component:HomePageComponent},
   {path:'bipartite',component:BipartiteComponent},
-  {path:'',redirectTo:'home-page',pathMatch:'full'},
-  {path:'publications',component:PublicationsComponent}
+  {path:'publications',component:PublicationsComponent},
+  {path:'',redirectTo:'/home-page',pathMatch:'full'}
 
 ]
 
@@ -34,7 +43,8 @@ const appRoutes:Routes=[
     BrowserModule,
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing:true}
+      {enableTracing:true,
+      useHash: true}
     )
   ],
   providers: [],
